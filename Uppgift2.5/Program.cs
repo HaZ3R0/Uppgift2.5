a4e786a8a9272582f60b7c7391ee954928569f03
majo06005 <majo06005@E-PW0234K2>
2023-09-15 13:42:41 +0200

    Add project files.

---
+++ Uppgift2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_2._5
+{
+    class Proram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en multiplication med två tal som båda har en decimal i sig.");
+            string multiplication = Console.ReadLine();
+
+            int gångerindex = multiplication.IndexOf("*");
+            string tal1text = multiplication[..gångerindex];
+            string tal2text = multiplication[(gångerindex + 1)..];
+            double tal1 = double.Parse(tal1text);
+            double tal2 = double.Parse(tal2text);
+            double summa = tal1 * tal2;
+            Console.WriteLine(multiplication + "=" + summa);
+            Console.ReadKey();
+        }
+    }
+}
